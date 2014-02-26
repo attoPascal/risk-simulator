7f67ab1c4f17786187556a0626790ddcc8c0405f
Pascal Attwenger <pascal.attwenger@gmail.com>
2014-02-27 00:25:05 +0100

    changed text

--- riskstrategy.py
+++ riskstrategy.py
@@ -8,7 +8,7 @@ PROBABILITY = int(argv[3]) if len(argv) > 3 else 75
 print("Objective:")
 print("win against", DEFENDER_UNITS, "opposing units")
 print("with at least", WINNING_UNITS, "units left")
-print("with a PROBABILITY of more than", PROBABILITY, "percent")
+print("with a probability of at least", PROBABILITY, "percent")
 print()
 
 my_units = 1
